cb16b4e1116a9e18ee23157039cf561cbe7d2d9f
Ivan Zakharov <vanya_z@list.ru>
2018-05-09 22:31:43 +1200

    Initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"todo-list/task"
+
+	"github.com/gorilla/handlers"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := task.NewRouter() // create routes
+
+	// These two lines are important in order to allow access from the front-end side to the methods
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+
+	// Launch server with CORS validations
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
+}
---
+++ task/router.go
@@ -0,0 +1,45 @@
+package task
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Route defines a route
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+// Routes defines the list of routes of our API
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/tasks",
+		AllTasksEndPoint,
+	},
+}
+
+// NewRouter configures a new router to the API
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
+		log.Println(route.Name)
+		handler = route.HandlerFunc
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+	return router
+}
